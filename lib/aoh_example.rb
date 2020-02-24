162f1b70ac8c80188e27313ca68170fd08a23661
Shau Yin Luk <ivan.luk@icloud.com>
2020-02-24 15:19:18 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -17,6 +17,7 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   aoh = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+  aoh
 end
 
 def aoh_lookup(aoh, row, key)
